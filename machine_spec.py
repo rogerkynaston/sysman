dcb0a89220c501c91d10cb2dd9a0a6cc6301c898
Kynaston <kynaston@scorpios.home>
2019-07-19 16:56:31 -0400

    added some more stuff

--- machine_spec.py
+++ machine_spec.py
@@ -1,4 +1,19 @@
+#!/usr/local/bin/python
 # This is the basic file that will generate information for the creation of the VM.
-sysname = input('Enter the machine name:')
-type = input('Enter the type of system')
-location = input('Enter the location')
+sysname = input('Enter the machine name. If machine name is not specific enter 0:')
+
+print ("Enter the Machine type")
+print ("1. Linux")
+print ("2. Windows")
+arch = input('Type:')
+
+print ("Enter location:")
+print ("1. AWS")
+print ("2. Azure")
+print ("3. Google")
+print ("4. Local")
+location = input('location')
+
+print ("Testing:", sysname)
+print ("Testing:", location)
+print ("Testing:", arch)
