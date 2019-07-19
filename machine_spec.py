5c5fba7fac97f97eb14504b688f60e4d2606af7a
Kynaston <kynaston@scorpios.home>
2019-07-19 16:40:23 -0400

    initial write

---
+++ machine_spec.py
@@ -0,0 +1,4 @@
+# This is the basic file that will generate information for the creation of the VM.
+sysname = input('Enter the machine name:')
+type = input('Enter the type of system')
+location = input('Enter the location')
